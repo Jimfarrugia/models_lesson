2f7f3f15c91b3da1d0a7898d0b35783b5b1a6618
Jim Farrugia <the.jim.farrugia@gmail.com>
2019-04-05 12:28:11 +1100

    added faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+for i in 1..20
+  Book.create(
+    title: Faker::Book.title,
+    author: Faker::Book.author,
+    description: Faker::Book.genre
+  )
+  puts "Created #{i} Books"
+end
